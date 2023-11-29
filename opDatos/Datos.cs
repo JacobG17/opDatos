b086450254ce69ffbea7cbc0954119535a8fbb73
Jacobo Gonzalez Soberanes <jacobo.gonzalez@coppel.com>
2023-11-29 10:31:27 -0700

    Desarrollo completo

---
+++ opDatos/Datos.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace opDatos
+{
+    public class Datos
+    {
+        string cadenaConexion;
+        
+        public void OpenConexion(ParametrosConexion parametros)
+        {
+            cadenaConexion = (parametros.obtenerCadena()).ToString();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(cadenaConexion);
+                connection.Open();
+                
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        public List<string> ObtieneDatos(ParametrosConexion parametros, string query)
+        {
+            var resultado = new List<string>();
+            cadenaConexion = (parametros.obtenerCadena()).ToString();
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string valor = reader.GetString(0);
+                        resultado.Add(valor);
+                    }
+                }
+                    
+                connection.Close();
+            } 
+            return resultado;
+        }
+
+        public void createBD(ParametrosConexion parametros, string nameBD)
+        {
+            string query = $"CREATE DATABASE {nameBD};";
+            cadenaConexion = (parametros.obtenerCadena()).ToString();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadenaConexion))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void deleteBD(ParametrosConexion parametros, string nameBD)
+        {
+            string query = $"DROP DATABASE {nameBD};";
+            cadenaConexion = (parametros.obtenerCadena()).ToString();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadenaConexion))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ opDatos/ParametrosConexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opDatos
+{
+    public class ParametrosConexion
+    {
+        StringBuilder parametroConexion = new StringBuilder();
+        public ParametrosConexion()
+        {
+            this.DataBase = "master";
+            this.Server = string.Empty;
+            this.User = string.Empty;
+            this.Password = string.Empty;
+        }
+
+        public string DataBase { get; set; }
+
+        public string Server { get; set; }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
+        public StringBuilder obtenerCadena()
+        {
+            parametroConexion.Clear();
+            parametroConexion.Append("Data Source=").Append(Server).Append(";");
+            parametroConexion.Append("Initial Catalog=master;");
+            parametroConexion.Append("User=").Append(User).Append(";");
+            parametroConexion.Append("Password=").Append(Password).Append(";");
+
+            return parametroConexion;
+        }
+
+    }
+}
